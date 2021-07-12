8a30610b4b32b08940f37af727c34c403661e89e
Valinor13 <jwcalhoun2@gmail.com>
2021-07-12 18:54:12 -0500

    eslint fixes

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,17 +1,16 @@
-/* eslint-disable */
-
+/* eslint-disable linebreak-style */
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-    return combination;
+  return combination;
 }
--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
-/* eslint-disable */
-
+/* eslint-disable linebreak-style */
+/* eslint-disable no-use-before-define */
 export default function taskBlock(trueOrFalse) {
-    if (trueOrFalse) {
-        task = true;
-        task2 = false;
-    }
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
-    var task = false;
-    var task2 = true;
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+  return [task, task2];
 }
