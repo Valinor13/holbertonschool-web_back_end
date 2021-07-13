f286426a89a61f2f25e3540b4c81eefa79863fd3
Valinor13 <jwcalhoun2@gmail.com>
2021-07-12 20:09:07 -0500

    Format changes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default function taskBlock(trueOrFalse) {
-  if (trueOrFalse) {
-    task = true;
-    task2 = false;
-  }
+    if (trueOrFalse) {
+        task = true;
+        task2 = false;
+    }
 
-  let task = false;
-  let task2 = true;
-  return [task, task2];
+    let task = false;
+    let task2 = true;
+    return [task, task2];
 }
