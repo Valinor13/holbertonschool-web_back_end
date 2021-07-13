ea8b2222456c659e170ac48035e52cad1dbc958a
Valinor13 <jwcalhoun2@gmail.com>
2021-07-12 20:28:58 -0500

    Eslint fixes again

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
+/* eslint-disable no-var */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-use-before-define */
 export default function taskBlock(trueOrFalse) {
-    if (trueOrFalse) {
-        task = true;
-        task2 = false;
-    }
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
-    var task = false;
-    var task2 = true;
-    return [task, task2];
+  var task = false;
+  var task2 = true;
+  return [task, task2];
 }
