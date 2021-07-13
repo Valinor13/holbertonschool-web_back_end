c4395bb55aa4308627718d51f16e246beae8f546
Valinor13 <jwcalhoun2@gmail.com>
2021-07-12 20:12:15 -0500

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,7 +5,7 @@ export default function taskBlock(trueOrFalse) {
         task2 = false;
     }
 
-    let task = false;
-    let task2 = true;
+    const task = false;
+    const task2 = true;
     return [task, task2];
 }
