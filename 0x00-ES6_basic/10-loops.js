82866bbc4bd57fd8293e984c37427812c6c3b19b
Valinor13 <jwcalhoun2@gmail.com>
2021-07-13 12:04:17 -0500

    Adding create report object

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,11 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArry = [];
-  for (const [idx, value] of array) {
-    if (array[idx] === value) {
-      newArry.push(appendString + value);
-    } else {
-      newArry.push(appendString + array[idx]);
-    }
+  for (const idx of array) {
+    newArry.push(appendString + array[idx]);
   }
   return newArry;
 }
