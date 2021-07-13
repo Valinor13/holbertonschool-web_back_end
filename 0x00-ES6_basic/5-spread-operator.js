a61d247c79627b4e8bf6a6dc33fe8ab8a3e8c80b
Valinor13 <jwcalhoun2@gmail.com>
2021-07-12 21:24:00 -0500

    I'm going through changes

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,7 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  const newArry = array1.concat(array2);
-  for (let i = 0; i < string.length; i += 1) {
-    newArry.push((string.charAt(i)));
-  }
+  const newArry = [...array1, ...array2, ...string];
   return newArry;
 }
