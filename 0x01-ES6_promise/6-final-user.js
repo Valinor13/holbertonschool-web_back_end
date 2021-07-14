8f40abe2ca09cefdace7107de6d6d4d42c82de09
Valinor13 <jwcalhoun2@gmail.com>
2021-07-14 17:07:59 -0500

    fixes to functions

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,13 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const retArr = [];
-  const userProm = signUpUser(firstName, lastName);
-  const picProm = uploadPhoto(fileName);
-  userProm.then((user) => {
-    console.log(user);
-  });
-  picProm.then((pics) => {
-    console.log(pics);
-  });
+  const promArr = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const p = Promise.all(promArr);
+  console.log(p);
+  retArr.push(p);
   return retArr;
 }
