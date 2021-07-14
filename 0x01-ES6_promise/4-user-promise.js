de2ea987a435f1fddf5c4c1806e6ce0cfa8979ee
Valinor13 <jwcalhoun2@gmail.com>
2021-07-14 12:44:41 -0500

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  const p = new Promise((resolve) => {
-    resolve({ firstName, lastName });
+  return Promise.resolve({
+    firstName,
+    lastName,
   });
-  return p;
 }
