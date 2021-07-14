ccbe22a418cde7e2bebfc64dd8555f6ed8a8b068
Valinor13 <jwcalhoun2@gmail.com>
2021-07-14 12:38:23 -0500

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,10 @@ import * as utils from './utils';
 export default function handleProfileSignup() {
   const pic = utils.uploadPhoto();
   const user = utils.createUser();
-  console.log(pic + user);
+  pic.then((pictures) => {
+    process.stdout.write(pictures.body.concat(' '));
+  });
+  user.then((users) => {
+    console.log(users.firstName, users.lastName);
+  });
 }
