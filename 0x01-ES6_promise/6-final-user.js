c2007f5eb5826dd4729f1f6ad5c1dc95975b8588
Valinor13 <jwcalhoun2@gmail.com>
2021-07-15 11:32:38 -0500

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-param-reassign */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promArr = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promArr).then((results) => results);
+  return Promise.allSettled(promArr).then((results) => {
+    results[1].value = results[1].reason;
+    delete results[1].reason;
+    results[1].value = results[1].value.message;
+  });
 }
