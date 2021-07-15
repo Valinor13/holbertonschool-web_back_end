00c62410a91d9a776bdc7c05fec6cdf702028ede
Valinor13 <jwcalhoun2@gmail.com>
2021-07-15 00:21:07 -0500

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promArr = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  const retArr = [];
-  Promise.allSettled(promArr)
-    .then((values) => retArr.push(values));
-  return retArr;
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values);
 }
