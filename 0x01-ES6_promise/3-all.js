2f967aed7e2bf2ad9d699a9a3a2d006f5e81a61d
Valinor13 <jwcalhoun2@gmail.com>
2021-07-14 23:57:49 -0500

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   const promArr = [uploadPhoto(), createUser()];
   Promise.all(promArr)
     .then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`))
-    .catch(() => console.error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
