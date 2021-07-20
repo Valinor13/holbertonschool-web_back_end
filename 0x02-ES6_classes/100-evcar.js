daddd1601ba203938bed48818e02f03166a23285
Valinor13 <jwcalhoun2@gmail.com>
2021-07-19 22:10:26 -0500

    Create 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+
+  // range.getter
+  get range() {
+    return this._range;
+  }
+
+  // brand.setter
+  set range(newRange) {
+    this._ramge = newRange;
+  }
+}
