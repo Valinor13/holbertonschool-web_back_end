0a2d2dbaa2b7c1b6948ef3a7a21f773cab5dd083
Valinor13 <jwcalhoun2@gmail.com>
2021-07-21 16:08:05 -0500

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,15 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-unused-vars */
 export default function cleanSet(inputSet, startString) {
-    
+  if (!(startString)) {
+    return '';
+  }
+  const retString = '';
+  function findString(element) {
+    if (element.startsWith(startString)) {
+      return retString.concat(element, '-');
+    }
+  }
+  inputSet.forEach(findString);
+  return retString.replace(startString, '');
 }
