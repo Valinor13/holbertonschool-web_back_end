7304e75977b782a84ec059edf45f72bd6243529e
Valinor13 <jwcalhoun2@gmail.com>
2021-11-02 13:02:23 -0500

    Adding git ignore updating add function to meet pepe standards

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module contains type annotated functions """
 
+
 def add(a: float, b: float):
     """ The add function returns 2 input floats added together """
     return float(a + b)
