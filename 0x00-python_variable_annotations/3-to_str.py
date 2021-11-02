ace926799e671f51dd51a84db79fd49db0f55285
Valinor13 <jwcalhoun2@gmail.com>
2021-11-02 15:14:33 -0500

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module contains type annotated functions """
+
+
+def to_str(n: float) -> str:
+    """ To String: Converts a float to a string
+            Params:
+                n: float - A decimal number
+            Return:
+                String representation of n """
+    return str(n)
