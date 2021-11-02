cd9d5cea881d7177c61051f0f8ce5c33534daf83
Valinor13 <jwcalhoun2@gmail.com>
2021-11-02 12:49:21 -0500

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+""" This module contains type annotated functions """
+
+def add(a: float, b: float):
+    """ The add function returns 2 input floats added together """
+    return float(a + b)
