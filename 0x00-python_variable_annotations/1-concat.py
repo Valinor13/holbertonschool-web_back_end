517ed437109a4edf863d90fb75d396aaa8f41f58
Valinor13 <jwcalhoun2@gmail.com>
2021-11-02 13:22:24 -0500

    Fixing variable names in concat, adding floor

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,11 +2,11 @@
 """ This module contains type annotated functions """
 
 
-def concat(a: str, b: str) -> str:
-    """ Concat:
+def concat(str1: str, str2: str) -> str:
+    """ Concat: Adds 2 strings together in sequence
             Params:
-                a: string - first half of new string
-                b: string - second half of new string
+                str1: string - first half of new string
+                str2: string - second half of new string
             Return:
                 New string a + b """
-    return a + b
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This module contains type annotated functions """
+
+
+def floor(n: float) -> int:
+    """ Floor: Identifies the next whole integer below a float
+            Params:
+                n: float - A decimal number
+            Return:
+                Integer below n """
+    return n
