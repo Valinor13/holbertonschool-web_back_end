164574cc3f536989d8d67c512d64c1a2efb70c37
Valinor13 <jwcalhoun2@gmail.com>
2021-11-02 13:17:26 -0500

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" This module contains type annotated functions """
+
+
+def concat(a: str, b: str) -> str:
+    """ Concat:
+            Params:
+                a: string - first half of new string
+                b: string - second half of new string
+            Return:
+                New string a + b """
+    return a + b
