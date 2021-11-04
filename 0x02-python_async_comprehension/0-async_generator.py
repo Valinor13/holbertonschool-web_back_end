a985652f7f7fc8231965337c24137da562e0f3bb
Valinor13 <jwcalhoun2@gmail.com>
2021-11-04 09:01:42 -0500

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
 
     """ Async Generator: Loops 10 times each time creating a new operation
                          that sleeps for 1 second and then yields a random
