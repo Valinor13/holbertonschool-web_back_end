6575aabfffdce0dbbc8db6d90db68f7f0c1ac915
Valinor13 <jwcalhoun2@gmail.com>
2021-11-04 19:13:17 -0500

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -11,12 +11,7 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
 
     """ Async Comprehension: Calls async_generator. Appends the yields to list
-            Variables:
-                result: list - list of float values interpreted from yields
             Return:
                 A list of yields converted to their float results """
 
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
