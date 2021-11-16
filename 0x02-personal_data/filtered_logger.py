f7b48afd9c4b772873d630a40700924953bce2e0
Valinor13 <jwcalhoun2@gmail.com>
2021-11-16 16:12:22 -0600

    Update filtered_logger.py

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -2,19 +2,35 @@
 """ A module that contains a filter for ; delimited strings """
 
 
+import logging
 import re
-from typing import List
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
+def filter_datum(fields, redaction, message, separator) -> str:
     """ Filter Datum filters the string for given fields """
-    messages = message.split(separator)
+    msgs = message.split(separator)
     for field in fields:
-        for i in range(0, len(messages)):
-            messages[i] = re.sub(field + '=[^ ]*' + '$',
-                                 field + '=' + redaction,
-                                 messages[i])
-    return separator.join(messages)
+        for i in range(len(msgs)):
+            msgs[i] = re.sub(f'{field}=.*', f'{field}={redaction}', msgs[i])
+    return separator.join(msgs)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the record """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
