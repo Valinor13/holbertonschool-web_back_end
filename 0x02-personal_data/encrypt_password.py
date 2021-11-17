add253f34e22a727615f8d75d9b53b2541f45957
Valinor13 <jwcalhoun2@gmail.com>
2021-11-17 16:51:22 -0600

    Update encrypt_password.py

--- 0x02-personal_data/encrypt_password.py
+++ 0x02-personal_data/encrypt_password.py
@@ -7,6 +7,6 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ Salt & Pepper a hashpass """
-    pdub = bytes(password)
+    pdub = b'{password}'
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(pdub, salt)
