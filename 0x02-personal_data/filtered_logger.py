2951a469ba27f574113aa594f452c462e7ae3401
Valinor13 <jwcalhoun2@gmail.com>
2021-11-17 10:16:38 -0600

    Update filtered_logger.py

--- 0x02-personal_data/filtered_logger.py
+++ 0x02-personal_data/filtered_logger.py
@@ -30,6 +30,7 @@ def get_logger() -> logging.Logger:
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
+    logger.propagate(False)
     return logger
 
 
