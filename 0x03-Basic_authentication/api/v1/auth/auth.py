97b52d011c30875dd0714e897c868083a22a80a4
Valinor13 <jwcalhoun2@gmail.com>
2021-11-22 12:45:09 -0600

    Create auth.py

---
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+from flask import request
+from typing import TypeVar
+
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
+DATA = {}
+
+
+class Auth():
+    """ Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require authentication check """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorized header req check """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns current user """
+        return None
