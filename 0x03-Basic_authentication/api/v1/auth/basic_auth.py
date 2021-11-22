3a61aa51a2ac72e52641730548400f70a77de968
Valinor13 <jwcalhoun2@gmail.com>
2021-11-22 15:44:28 -0600

    Update basic_auth.py

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -19,3 +19,15 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic ') is False:
             return None
         return (authorization_header[6:])
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+        comp_str = base64.b64decode(base64_authorization_header)
+        if base64_authorization_header == base64.b64encode(comp_str):
+            return comp_str
+        return None
