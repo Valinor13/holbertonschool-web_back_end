54d0e6a0f630960e825bbb5f452f745ebcc3850f
Valinor13 <jwcalhoun2@gmail.com>
2021-11-22 13:53:12 -0600

    Update auth.py

--- 0x03-Basic_authentication/api/v1/auth/auth.py
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -5,17 +5,20 @@ from flask import request
 from typing import TypeVar, List
 
 
-TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S"
-DATA = {}
-
-
 class Auth():
     """ Auth class
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require authentication check """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for item in excluded_paths:
+            if path in item or (path + '/') in item:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorized header req check """
