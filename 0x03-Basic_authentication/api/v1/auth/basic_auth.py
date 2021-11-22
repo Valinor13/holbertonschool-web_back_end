8711757a79ce85fbcab4875ed0943ce1d9ed23f1
Valinor13 <jwcalhoun2@gmail.com>
2021-11-22 15:24:06 -0600

    Update basic_auth.py

--- 0x03-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x03-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-""" Basic Auth module
-"""
+""" Basic Auth module """
 from flask import request
 from typing import TypeVar, List
 from auth import Auth
 
 
 class BasicAuth(Auth):
-    """ Basic Auth class
-    """
+    """ Basic Auth class """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract Base64 method """
+        if authorization_header is None:
+            return None
