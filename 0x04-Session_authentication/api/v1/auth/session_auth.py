e21b353355319d9d2d475c07ec44cff92806ebc2
Valinor13 <jwcalhoun2@gmail.com>
2021-11-23 16:50:34 -0600

    Update session_auth.py

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -18,4 +18,10 @@ class SessionAuth(Auth):
         """ Creates a session id and assigns it to the user id """
         if not user_id or not isinstance(user_id, str):
             return None
-        user_id_by_session_id[str(uuid.uuid4())] = user_id
+        self.user_id_by_session_id[str(uuid.uuid4())] = user_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Getter method for user id based on session id """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
