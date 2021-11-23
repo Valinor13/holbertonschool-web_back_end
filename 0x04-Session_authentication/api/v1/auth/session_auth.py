c39a24f8748d74c701f3fe586d7535f5d393754b
Valinor13 <jwcalhoun2@gmail.com>
2021-11-23 16:46:22 -0600

    Update session_auth.py

--- 0x04-Session_authentication/api/v1/auth/session_auth.py
+++ 0x04-Session_authentication/api/v1/auth/session_auth.py
@@ -6,7 +6,15 @@ from typing import List, Tuple, TypeVar
 from models.user import User
 from api.v1.auth.auth import Auth
 import base64
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Auth class inheriting from Auth base """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if not user_id or not isinstance(user_id, str):
+            return None
+        user_id_by_session_id[str(uuid.uuid4())] = user_id
