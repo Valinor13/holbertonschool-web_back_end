3167e55cfb869fc254e4ed7d264fc06652637583
Valinor13 <jwcalhoun2@gmail.com>
2021-11-24 21:15:18 -0600

    Update session_auth.py

--- 0x04-Session_authentication/api/v1/views/session_auth.py
+++ 0x04-Session_authentication/api/v1/views/session_auth.py
@@ -35,7 +35,9 @@ def login() -> str:
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
 def logout() -> dict():
     """ Deletes session from cookies """
     from api.v1.app import auth
