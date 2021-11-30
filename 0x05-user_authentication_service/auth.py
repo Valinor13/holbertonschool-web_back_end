345fac994e5846a18461545ca863c4bca962b496
Valinor13 <jwcalhoun2@gmail.com>
2021-11-30 16:33:58 -0600

    Update auth.py

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 """ This module hashes a password and returns it in bytes """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Verifies new user to be stored in database prior to auth """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User <{email}> already exists')
+        except NoResultFound:
+            hp = _hash_password(password)
+            return self._db.add_user(email, hp)
 
 
 def _hash_password(password: str) -> bytes:
