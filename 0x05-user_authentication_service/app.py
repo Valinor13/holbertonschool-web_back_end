0e49e00617b9c8c7f05c64c0f0c4340a8b2d4f31
Valinor13 <jwcalhoun2@gmail.com>
2021-11-30 17:13:12 -0600

    Update app.py

--- 0x05-user_authentication_service/app.py
+++ 0x05-user_authentication_service/app.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 """ Appy flask navigator module """
 from flask import Flask, jsonify
+from auth import Auth
+import requests
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome_message() -> str:
-    """ GET /api/v1/status
+    """ GET home dir
     Return:
-      - the status of the API
+      - welcome message
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ POST users
+    Return:
+      - 
+    """
+    pw = requests.form.get('password')
+    email = requests.form.get('email')
+
+    try:
+      AUTH.register_user(email, pw)
+      return jsonify({'email': email, 'message': 'user created'})
+    except ValueError:
+      return jsonify({'message': 'email already registered'}), 400
 
 
 if __name__ == "__main__":
