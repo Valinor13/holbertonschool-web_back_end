76de22169b1ea41f6b248546342875260faa346e
Valinor13 <jwcalhoun2@gmail.com>
2021-11-30 14:37:54 -0600

    Update user.py

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -17,6 +17,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        return f"<User(email={self.email}, session_id={self.session_id})>"
