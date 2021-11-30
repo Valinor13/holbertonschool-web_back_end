3bfc2eeb8d34591ed9265c2d5190d17296f0f86b
Valinor13 <jwcalhoun2@gmail.com>
2021-11-30 15:39:54 -0600

    Update db.py

--- 0x05-user_authentication_service/db.py
+++ 0x05-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -36,3 +37,13 @@ class DB:
         self._session.add(newUser)
         self._session.commit()
         return newUser
+
+    def find_user_by(self, **kwargs):
+        """ """
+        try:
+            q = self._session.query(User).filter_by(**kwargs).first()
+            if not q:
+                raise NoResultFound
+            return q
+        except InvalidRequestError:
+            raise InvalidRequestError
