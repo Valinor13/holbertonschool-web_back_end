feaa5154f3b6145c5ac32321cb0038ee6a9309d1
Valinor13 <jwcalhoun2@gmail.com>
2021-12-01 15:57:24 -0600

    Update auth.py

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -77,9 +77,8 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashpw = _hash_password(password)
-            self._db.update_user(user.id,
-                                 reset_token=None,
-                                 hashed_password=hashpw)
+            self._db.update_user(user.id, reset_token=None)
+            self._db.update_user(user.id, hashed_password=hashpw)
         except Exception:
             raise ValueError
 
