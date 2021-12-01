6dddc1bf006152c114696a400fe8c3bc12d64a4b
Valinor13 <jwcalhoun2@gmail.com>
2021-12-01 14:35:50 -0600

    Update auth.py

--- 0x05-user_authentication_service/auth.py
+++ 0x05-user_authentication_service/auth.py
@@ -52,7 +52,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """ Destroys user session """
         try:
