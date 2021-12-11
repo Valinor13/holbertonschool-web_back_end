c1601847814a8435054c704d28b5154a58c28b15
Valinor13 <jwcalhoun2@gmail.com>
2021-12-11 14:36:54 -0600

    Create 2-app.py

---
+++ 0x01-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" This is the basic flask appy """
+
+from flask import Flask, request
+from flask_babel import Babel
+from flask.templating import render_template
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config settings for babel app """
+    LANGUAGES = ['en', 'fr']
+
+    @babel.localeselector
+    def get_locale(self):
+        """ returns the location for the g11n app """
+        return request.accept_languages.best_match(app.config(self.LANGUAGES))
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ calls the index from templates """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
