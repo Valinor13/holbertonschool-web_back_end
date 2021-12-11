8b799b403551a308d7b23bc4d44e8361637cb5dc
Valinor13 <jwcalhoun2@gmail.com>
2021-12-11 13:03:01 -0600

    Update 0-app.py

--- 0x01-i18n/0-app.py
+++ 0x01-i18n/0-app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ calls the index from templates """
-    return render_template('/templates/0-index.html')
+    return render_template('templates/0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
