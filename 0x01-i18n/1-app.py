c049a03cb0c720becd08b453a91fb7d39f3b0d0f
Valinor13 <jwcalhoun2@gmail.com>
2021-12-11 14:36:56 -0600

    Update 1-app.py

--- 0x01-i18n/1-app.py
+++ 0x01-i18n/1-app.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
     """ calls the index from templates """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
