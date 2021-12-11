6729f64876728a38df670d908808ee928533cbdb
Valinor13 <jwcalhoun2@gmail.com>
2021-12-11 14:17:35 -0600

    Update 1-app.py

--- 0x01-i18n/1-app.py
+++ 0x01-i18n/1-app.py
@@ -15,7 +15,7 @@ class Config:
     @babel.localeselector
     def get_locale(self):
         """ returns the location for the g11n app """
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+        return request.accept_languages.best_match(self.LANGUAGES)
 
     @babel.timezoneselector
     def get_timezone(self):
