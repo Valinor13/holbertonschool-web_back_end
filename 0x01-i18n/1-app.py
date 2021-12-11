fdef3e05e90a9638470671eced7237f41a63f944
Valinor13 <jwcalhoun2@gmail.com>
2021-12-11 14:16:26 -0600

    Update 1-app.py

--- 0x01-i18n/1-app.py
+++ 0x01-i18n/1-app.py
@@ -15,7 +15,12 @@ class Config:
     @babel.localeselector
     def get_locale(self):
         """ returns the location for the g11n app """
-        return request.accept_languages.best_match(self.LANGUAGES)
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    @babel.timezoneselector
+    def get_timezone(self):
+        """ returns the timezone default = UTC """
+        return 'UTC'
 
 
 @app.route('/')
