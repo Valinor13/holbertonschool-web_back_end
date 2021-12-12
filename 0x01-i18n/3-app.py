05947baed022d30422c246de2477a62251719734
Valinor13 <jwcalhoun2@gmail.com>
2021-12-12 13:55:21 -0600

    Task 3 feeling appy

---
+++ 0x01-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" This is the basic flask appy """
+
+from flask import Flask, request
+from flask_babel import Babel
+from flask.templating import render_template
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config settings for babel app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """ returns the location for the g11n app """
+    return request.accept_languages.best_match(app.config(Config.LANGUAGES))
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ calls the index from templates """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
