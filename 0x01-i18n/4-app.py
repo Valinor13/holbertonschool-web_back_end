9992f03bc2e12f6f33a0a30be404bea9142555df
Valinor13 <jwcalhoun2@gmail.com>
2021-12-12 15:00:24 -0600

    Create 4-app.py

---
+++ 0x01-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" This is the basic flask appy """
+
+from flask import Flask, request
+from flask_babel import Babel
+from flask.templating import render_template
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Config settings for babel app """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+@babel.localeselector
+def get_locale():
+    """ returns the location for the g11n app """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    t10s = [str(translation) for translation in babel.list_translations()]
+    return request.accept_languages.best_match(t10s)
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ calls the index from templates """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
