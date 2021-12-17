6a1418b4a6549ab5bc6a10347b84da754bd6b8a5
Jay Calhoun <jwcalhoun2@gmail.com>
2021-12-17 03:05:56 +0000

    missing THEN

--- 0x01-MySQL_Advanced/5-valid_email.sql
+++ 0x01-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ DELIMITER |
     BEFORE UPDATE
     ON users FOR EACH ROW
     BEGIN
-        IF NEW.email != OLD.email
+        IF NEW.email != OLD.email THEN
             SET NEW.valid_email = 0;
         END IF;
     END;
