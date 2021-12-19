7f7a31470a67fadd426e46dfcea6ad99f09e0998
Valinor13 <jwcalhoun2@gmail.com>
2021-12-18 22:53:50 -0600

    Update 6-bonus.sql

--- 0x01-MySQL_Advanced/6-bonus.sql
+++ 0x01-MySQL_Advanced/6-bonus.sql
@@ -7,16 +7,21 @@ CREATE PROCEDURE AddBonus
     IN id INT, IN project_name VARCHAR(255), IN score INT
 )
 BEGIN
-    INSERT IGNORE INTO projects (name)
-    VALUES (project_name);
-    SELECT id AS project_id
-    FROM projects
-    WHERE name = project_name;
-    INSERT IGNORE INTO corrections (user_id, project_id)
-    VALUES (id, project_id);
-    UPDATE corrections
-    SET score = score
-    WHERE project_id = project_id;
+    SET @project_id := (
+        SELECT id
+        FROM projects
+        WHERE name = project_name;
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name)
+        VALUES (project_name)
+    END IF;
+    SET @project_id := (
+        SELECT id
+        FROM projects
+        WHERE name = project_name;
+    )
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (id, project_id, score)
 END;
 //
 DELIMITER ;
