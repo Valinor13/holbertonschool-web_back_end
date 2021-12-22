cf715b03d2c0af40d831b725a03651f9bf3c44d9
Jay Calhoun <jwcalhoun2@gmail.com>
2021-12-21 17:53:06 -0800

    Updating find

--- 0x02-NoSQL/11-schools_by_topic.py
+++ 0x02-NoSQL/11-schools_by_topic.py
@@ -6,8 +6,4 @@ import pymongo
 
 def schools_by_topic(mongo_collection, topic):
     """ list schools be topic """
-    school_list = []
-    for record in mongo_collection.find():
-        if topic in record.get("topics"):
-            school_list.append(record.get("name"))
-    return school_list
+    return mongo_collection.find({"topics": topic})
