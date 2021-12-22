284f7339550e0c8d1a407c6d663c0451de0c1b4a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-12-21 16:38:19 -0800

    Adding doc to collection via kwargs

--- 0x02-NoSQL/9-insert_school.py
+++ 0x02-NoSQL/9-insert_school.py
@@ -6,7 +6,4 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """ insert new record into collection """
-    new_doc = {}
-    for k, v in kwargs.items():
-        new_doc[k] = v
-    return mongo_collection.insert_one(new_doc)
+    return mongo_collection.insert_one(kwargs).inserted_id
