4faa9602ce24a2415f911477613b7af2b3a21cfb
Valinor13 <jwcalhoun2@gmail.com>
2022-01-04 17:57:26 -0600

    Create 2-read_file.js

---
+++ 0x00-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+function countStudents(path) {
+  try {
+    const fieldsList = [];
+    const studentsByField = [];
+    const file = require('fs').readFileSync(path, { encoding: 'utf8' });
+    const studentList = file.split('\r\n');
+    studentList.shift();
+    console.log(`Number of students: ${studentList.length}`);
+    studentList.forEach((student) => {
+      const field = student.split(',');
+      fieldsList.push(field[3]);
+    });
+    const fieldSet = new Set(fieldsList);
+    let i = 0;
+    fieldSet.forEach((field) => {
+      studentsByField.push([]);
+      studentList.forEach((student) => {
+        const fields = student.split(',');
+        if (field === fields[3]) {
+          studentsByField[i].push(fields[0]);
+        }
+      });
+      i += 1;
+    });
+    let j = 0;
+    fieldSet.forEach((field) => {
+      console.log(`Number of students in ${field}: ${studentsByField[j].length}. List: ${studentsByField[j].join(', ')}`);
+      j += 1;
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
