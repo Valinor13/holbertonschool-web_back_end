183b27fb4392e178f04ca9798bef30eb183c2842
Valinor13 <jwcalhoun2@gmail.com>
2022-01-05 11:34:59 -0600

    Update 1-stdin.js

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -8,5 +8,5 @@ readline.question('Welcome to Holberton School, what is your name?\n', (name) =>
 });
 
 process.on('beforeExit', () => {
-  readline.write('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
