0d5629e6f6261201c343f1c32f33fddf93beb826
Valinor13 <jwcalhoun2@gmail.com>
2022-01-04 18:24:44 -0600

    Create 3-read_file_async.js

---
+++ 0x00-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+async function countStudents(path) {
+  const fs = require('fs').promises;
+  try {
+    const fieldsList = [];
+    const studentsByField = [];
+    const file = await fs.readFile(path, { encoding: 'utf8' });
+    const studentList = file.split('\r\n');
+    studentList.shift();
+    console.log(`Number of students: ${studentList.length}`);
+    studentList.forEach((student) => {
+      const field = student.split(',');
+      fieldsList.push(field[3]);
+    });
+    const fieldSet = new Set(fieldsList);
+    let i = 0;
+    fieldSet.forEach((field) => {
+      studentsByField.push([]);
+      studentList.forEach((student) => {
+        const fields = student.split(',');
+        if (field === fields[3]) {
+          studentsByField[i].push(fields[0]);
+        }
+      });
+      i += 1;
+    });
+    let j = 0;
+    fieldSet.forEach((field) => {
+      console.log(`Number of students in ${field}: ${studentsByField[j].length}. List: ${studentsByField[j].join(', ')}`);
+      j += 1;
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
