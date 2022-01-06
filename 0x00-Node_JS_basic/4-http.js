824eec899acebdf03f635bacf908e1975f5c1173
Valinor13 <jwcalhoun2@gmail.com>
2022-01-05 22:42:36 -0600

    Update 4-http.js

--- 0x00-Node_JS_basic/4-http.js
+++ 0x00-Node_JS_basic/4-http.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
 const port = 1245;
-const app = http.createServer((res) => {
-  res.end('Hello Holberton School!');
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
 });
 
 app.listen(port, (err) => {
