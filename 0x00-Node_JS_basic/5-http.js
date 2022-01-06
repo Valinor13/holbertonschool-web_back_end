b3ea3a3070642de3f91f75aa932dcd8647bfb657
Valinor13 <jwcalhoun2@gmail.com>
2022-01-05 23:06:04 -0600

    Update 5-http.js

--- 0x00-Node_JS_basic/5-http.js
+++ 0x00-Node_JS_basic/5-http.js
@@ -1,9 +1,20 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
-const app = http.createServer((res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (req.url === '/students') {
+    const studentRes = await countStudents(process.argv[2]);
+    res.write('This is the list of our students');
+    studentRes.forEach((element) => {
+      res.write(element);
+    });
+    res.end();
+  }
 });
 
 app.listen(port, (err) => {
