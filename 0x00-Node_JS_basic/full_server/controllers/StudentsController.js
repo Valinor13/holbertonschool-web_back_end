92df0adc7c76e7e79815bcc2a955ec7262cfb7a7
Valinor13 <jwcalhoun2@gmail.com>
2022-01-06 23:37:46 -0600

    Create StudentsController.js

---
+++ 0x00-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,26 @@
+import readDatabase from 'utils';
+
+class StudentsController {
+  async static getAllStudents(request, response) {
+    response.write('This is the list of our students');
+    try {
+      if (process.argv[2] === '') {
+        throw new Error();
+      }
+      const studentRes = await readDatabase(process.argv[2]);
+      studentRes.forEach((element) => {
+        res.write(element.concat('\n'));
+      });
+    } catch (error) {
+      res.write('Cannot load the database');
+      res.status(500);
+    }
+    res.end();
+  }
+
+  async static getAllStudentsByMajor(request, response) {
+    return 'Major parameter must be CS or SWE', 500;
+  }
+}
+
+module.exports = StudentsController;
