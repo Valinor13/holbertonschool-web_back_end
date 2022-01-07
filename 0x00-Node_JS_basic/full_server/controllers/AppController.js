445e267a9cce307d3a38de336e6116ed2b6f7264
Valinor13 <jwcalhoun2@gmail.com>
2022-01-06 23:45:04 -0600

    Update AppController.js

--- 0x00-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x00-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,6 @@
 class AppController {
-  static getHomepage() {
-    return 'Hello Holberton School!';
+  static getHomepage(request, response) {
+    return response.status(200).send('Hello Holberton School!');
   }
 }
 
