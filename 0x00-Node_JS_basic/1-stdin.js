50814930f4c1cff41293250a3aa15c10e3bc9c8a
Valinor13 <jwcalhoun2@gmail.com>
2022-01-08 23:23:18 -0600

    Update 1-stdin.js

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -1,7 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  console.log(`Your name is: ${input}`);
+  if (input) {
+    console.log(`Your name is: ${input}`);
+  }
 });
 
 process.stdin.on('end', () => {
