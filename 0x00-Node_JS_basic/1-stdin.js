4ffec64fcdd3d3ebd4c3eafbe6e1761239ffb54f
Valinor13 <jwcalhoun2@gmail.com>
2022-01-08 23:24:38 -0600

    Update 1-stdin.js

--- 0x00-Node_JS_basic/1-stdin.js
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -7,5 +7,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
