1cd90679e410d544d58bc879110d9b1c027e4315
Valinor13 <jwcalhoun2@gmail.com>
2022-01-11 13:10:00 -0600

    Update 3-payment.test.js

---
+++ 0x01-unittests_in_js/3-payment.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', function() {
+    it('Test to use function', function() {
+        expect(sendPaymentRequestToApi(100, 20)).to.equal(120);
+    });
+    it('Spy the use of callback function', function() {
+        const spy = sinon.spy(Utils.calculateNumber, 'utils function');
+        sendPaymentRequestToApi(100, 20);
+        sinon.assert.calledOnce(spy);
+    });
+});
