f10a5a012c14e173e72b76405b0e3315789fdb53
Valinor13 <jwcalhoun2@gmail.com>
2022-01-11 12:35:32 -0600

    Update 2-calcul_chai.test.js

--- 0x01-unittests_in_js/2-calcul_chai.test.js
+++ 0x01-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,23 @@
 const chai = require('chai');
-const calculateNumber = require("./1-calcul");
+const calculateNumber = require("./2-calcul_chai");
 
 describe('calculateNumber SUM', function() {
     it('should determine if calculateNumber returns the correct sum', function() {
-        const num = calculateNumber(4, 5, 'SUM');
+        const num = calculateNumber('SUM', 4, 5);
         chai.expect(num).to.equal(9, '4.0 + 5.0 = 9');
     })
 })
 
 describe('calculateNumber SUBTRACT', function() {
     it('should determine if calculateNumber returns the correct subtraction', function() {
-        const num = calculateNumber(5, 1, 'SUBTRACT');
+        const num = calculateNumber('SUBTRACT', 5, 1);
         chai.expect(num).to.equal(4, '5.0 - 1.0 = 4');
     })
 })
 
 describe('calculateNumber DIVIDE', function() {
     it('should determine if calculateNumber returns the correct division', function() {
-        const num = calculateNumber(5, 5, 'DIVIDE');
+        const num = calculateNumber('DIVIDE', 5, 5);
         chai.expect(num).to.equal(1, '5.0 / 5.0 = 1');
     })
 })
