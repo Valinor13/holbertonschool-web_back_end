65b7d94c984b98cf807003c171a08dc0b30f3cc0
Valinor13 <jwcalhoun2@gmail.com>
2022-01-11 12:27:21 -0600

    Update 0-calcul.test.js

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -7,3 +7,24 @@ describe('calculateNumber', function() {
         assert.equal(num, 9, '4.0 + 5.0 = 9');
     })
 })
+
+describe('calculateNumber aRounded', function() {
+    it('should determine if calculateNumber returns the correct value', function() {
+        const num = calculateNumber(4.4, 5);
+        assert.equal(num, 9, '4.4 + 5.0 = 9');
+    })
+})
+
+describe('calculateNumber bRounded', function() {
+    it('should determine if calculateNumber returns the correct value', function() {
+        const num = calculateNumber(4.0, 5.6);
+        assert.equal(num, 10, '4.0 + 5.6 = 10');
+    })
+})
+
+describe('calculateNumber bothRounded', function() {
+    it('should determine if calculateNumber returns the correct value', function() {
+        const num = calculateNumber(4.7, 5.5);
+        assert.equal(num, 11, '4.7 + 5.5 = 11');
+    })
+})
