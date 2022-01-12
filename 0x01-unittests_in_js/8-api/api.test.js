63d49837dbcb6a727d1750629ac2e87cb57a1f66
Valinor13 <jwcalhoun2@gmail.com>
2022-01-11 22:14:26 -0600

    Update api.test.js

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -7,7 +7,7 @@ describe("getIndexPage", function() {
     it("should return index page", function(done) {
       request('http://localhost:7865', function(err, res, body) {
         expect(res.statusCode).to.equal(200);
-        expect(res.body).to.equal('Welcome to the payment system');
+        expect(body).to.equal('Welcome to the payment system');
         done();
       })
     });
