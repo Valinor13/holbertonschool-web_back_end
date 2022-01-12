b14d50a6bc69c2025af953c702cdfe1aacf190ba
Valinor13 <jwcalhoun2@gmail.com>
2022-01-11 21:48:42 -0600

    Update api.test.js

--- 0x01-unittests_in_js/8-api/api.test.js
+++ 0x01-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,14 @@
-const getIndexPage = require('./api');
+const app = require('./api');
 const sinon = require('sinon');
-const expect = require('chai').expect;
+const request = require('chai').request;
 
 describe("getIndexPage", function() {
-    it("should return index page", function() {
-      let req = {}
-      let res = {
-        send: sinon.spy()
-      }
-      getIndexPage(req, res);
-      console.log(res.send);
+    it("should return index page", function(done) {
+      chai.request(app).get('/')
+      .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.eq('Welcome to the payment system')
+          done();
+      });
     });
 });
