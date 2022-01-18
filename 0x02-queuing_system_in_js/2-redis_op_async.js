c4422249a8bc0e5a4251a4842b2f8cc2247320f8
Valinor13 <jwcalhoun2@gmail.com>
2022-01-18 10:28:05 -0600

    Create 2-redis_op_async.js

---
+++ 0x02-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,19 @@
+const redis = require('redis');
+
+const client = redis.createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  await client.get(schoolName, (err, data) => console.log(data));
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+})();
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
