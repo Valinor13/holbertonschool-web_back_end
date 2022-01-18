e464d7907f15528c6932bc0d95e36e93c87ebf3b
Valinor13 <jwcalhoun2@gmail.com>
2022-01-18 10:14:04 -0600

    Create 1-redis_op.js

---
+++ 0x02-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,17 @@
+import { redis, createClient } from 'redis';
+
+const client = createClient();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('ready', () => console.log('Redis client connected to the server'));
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, data) => console.log(data));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
