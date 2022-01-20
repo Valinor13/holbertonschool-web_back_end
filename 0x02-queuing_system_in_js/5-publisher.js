d3c5b2f068fb1c7bad52f750e5331b0e20221680
Valinor13 <jwcalhoun2@gmail.com>
2022-01-19 22:43:40 -0600

    Create 5-publisher.js

---
+++ 0x02-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,18 @@
+const redis = require('redis');
+
+const pub = redis.createClient();
+pub.on('error', (err) => console.log('Redis client not connected to the server:', err));
+pub.on('ready', () => console.log('Redis client connected to the server'));
+
+function publishMessage(message, time) {
+  async () => {
+    await sleep(time);
+  }
+  console.log('About to send MESSAGE');
+  pub.publish('holberton school channel', message);
+}
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
