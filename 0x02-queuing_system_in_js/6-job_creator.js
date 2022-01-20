add3ea68b5f602b9188c9ddb4183b61f38e02c2d
Valinor13 <jwcalhoun2@gmail.com>
2022-01-19 23:17:19 -0600

    Update 6-job_creator.js

--- 0x02-queuing_system_in_js/6-job_creator.js
+++ 0x02-queuing_system_in_js/6-job_creator.js
@@ -5,6 +5,7 @@ const data = {
   phoneNumber: String,
   message: String,
 };
-const job = que.create('push_notification_code', data, () => console.log(`Notification job created: ${job.id}`));
+const job = que.create('push_notification_code', data);
+console.log(`Notification job created: ${job.id}`);
 
 job.on('complete', () => console.log('Notification job completed')).on('failed', () => console.log('Notification job failed'));
