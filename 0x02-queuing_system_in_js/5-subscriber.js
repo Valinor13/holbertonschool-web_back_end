a25ef4b7c6ca7295c182e2f61c1b7a00f550b293
Valinor13 <jwcalhoun2@gmail.com>
2022-01-19 22:43:41 -0600

    Create 5-subscriber.js

---
+++ 0x02-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+
+const sub = redis.createClient();
+sub.on('error', (err) => console.log('Redis client not connected to the server:', err));
+sub.on('ready', () => console.log('Redis client connected to the server'));
+
+sub.on('message', (channel, message) => {
+    if (message === 'KILL_SERVER') {
+        sub.unsubscribe('holberton school channel');
+        sub.quit();
+    }
+    console.log(message);
+});
+
+sub.subscribe('holberton school channel');
