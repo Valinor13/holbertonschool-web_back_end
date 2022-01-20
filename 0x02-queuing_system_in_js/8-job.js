3e1c9258b2d1a2f77d625648322dbfd1b8e47bab
Jay Calhoun <jwcalhoun2@gmail.com>
2022-01-20 16:50:35 -0600

    adding from vim

--- 0x02-queuing_system_in_js/8-job.js
+++ 0x02-queuing_system_in_js/8-job.js
@@ -1,19 +1,18 @@
 function createPushNotificationsJobs(jobs, queue) {
-    if (typeof jobs !== 'array') {
-        throw new Error('Jobs is not an array');
-    }
+  if (Array.isArray(jobs) {
     jobs.forEach(data => {
-        let job = queue.create('push_notification_code_3', data).save((err) => {
-            if (!err) {
-              console.log(`Notification job created: #${job.id}`);
-            }
-          });
-          job.on('complete', () => {
-            console.log(`Notification job #${job.id} completed`)
-          }).on('progress', (progress) => {
-            console.log(`Notification job #${job.id} ${progress}% complete`)
-          }).on('failed', (err) => {
-            console.log(`Notification job #${job.id} failed: ${err}`)
-          });
+      let job = queue.create('push_notification_code_3', data).save((err) => {
+        if (!err) {
+          console.log(`Notification job created: #${job.id}`);
+        }
+      });
+      job.on('complete', () => {
+        console.log(`Notification job #${job.id} completed`)
+      }).on('progress', (progress) => {
+        console.log(`Notification job #${job.id} ${progress}% complete`)
+      }).on('failed', (err) => {
+        console.log(`Notification job #${job.id} failed: ${err}`)
+      });
     });
+  };
 };
