cc530d47bd0d2769045edb9dd201e0268d1ce871
Valinor13 <jwcalhoun2@gmail.com>
2022-01-19 23:40:03 -0600

    Update 6-job_creator.js

--- 0x02-queuing_system_in_js/6-job_creator.js
+++ 0x02-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue')
 
 const que = kue.createQueue();
 const data = {
-  phoneNumber: String,
-  message: String,
+  phoneNumber: '8675309',
+  message: 'Oh, you don\'t know me, but you make me so happy',
 };
 const job = que.create('push_notification_code', data).save(() => console.log(`Notification job created: ${job.id}`));
 
