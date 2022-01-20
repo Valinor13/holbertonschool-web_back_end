bcb52c956da3399d53f88581a5eb4b349a022640
Valinor13 <jwcalhoun2@gmail.com>
2022-01-19 22:43:38 -0600

    Update 0-redis_client.js

--- 0x02-queuing_system_in_js/0-redis_client.js
+++ 0x02-queuing_system_in_js/0-redis_client.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 (async () => {
   const client = createClient();
 
-  client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+  client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
 
   await client.on('ready', () => console.log('Redis client connected to the server'));
 })();
