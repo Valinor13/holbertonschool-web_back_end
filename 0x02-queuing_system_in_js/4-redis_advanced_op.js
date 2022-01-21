bb70ea16da45a9cf449069cc7cb134e0a7d5f079
Valinor13 <jwcalhoun2@gmail.com>
2022-01-21 10:53:48 -0600

    Update 4-redis_advanced_op.js

--- 0x02-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x02-queuing_system_in_js/4-redis_advanced_op.js
@@ -4,5 +4,5 @@ const client = redis.createClient();
 client.on('error', (err) => console.log('Redis client not connected to the server:', err));
 client.on('ready', () => console.log('Redis client connected to the server'));
 
-client.hmset('HolbertonSchools', 'Portland', 50, 'Seattle', 80, 'New York', 20, 'Bogota', 20, 'Cali', 40, 'Paris', 2, redis.print);
+client.hmset('HolbertonSchools', 'Tulsa', 50, 'New Haven', 80, 'San Fran', 20, 'Bogota', 20, 'Cali', 40, 'Paris', 2, redis.print);
 client.hgetall('HolbertonSchools', (err, data) => console.log(data));
