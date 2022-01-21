98a31c9c10a675a9800da467850b72daf5a3b450
Valinor13 <jwcalhoun2@gmail.com>
2022-01-20 18:38:59 -0600

    Update 8-job.test.js

--- 0x02-queuing_system_in_js/8-job.test.js
+++ 0x02-queuing_system_in_js/8-job.test.js
@@ -3,6 +3,16 @@ const expect = require('chai').expect;
 import createPushNotificationsJobs from './8-job';
 
 const que = kue.createQueue();
+const dataList = [
+    {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+    },
+    {
+        phoneNumber: '1234567890',
+        message: 'This is test job 2'
+    }
+];
 
 describe('functionality that deals with kue', () => {
     before(() => que.testMode.enter());
@@ -10,17 +20,7 @@ describe('functionality that deals with kue', () => {
     after(() => que.testMode.exit());
     
     it('enques a job validating correct data', () => {
-        const list = [
-            {
-                phoneNumber: '4153518780',
-                message: 'This is the code 1234 to verify your account'
-            },
-            {
-                phoneNumber: '1234567890',
-                message: 'This is test job 2'
-            }
-        ];
-        createPushNotificationsJobs(list, que);
+        createPushNotificationsJobs(dataList, que);
         expect(que.testMode.jobs.length).to.equal(2);
         expect(que.testMode.jobs[0].type).to.equal('push_notification_code_3');
         expect(que.testMode.jobs[0].data).to.eql({
