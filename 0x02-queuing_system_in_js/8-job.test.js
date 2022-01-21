69cce35b3d5170a0f1fcc659e139eb6fce0e4868
Valinor13 <jwcalhoun2@gmail.com>
2022-01-20 18:36:46 -0600

    Update 8-job.test.js

--- 0x02-queuing_system_in_js/8-job.test.js
+++ 0x02-queuing_system_in_js/8-job.test.js
@@ -1,5 +1,6 @@
-const expect = require('chai').expect;
 const kue = require('kue');
+const expect = require('chai').expect;
+import createPushNotificationsJobs from './8-job';
 
 const que = kue.createQueue();
 
@@ -9,7 +10,6 @@ describe('functionality that deals with kue', () => {
     after(() => que.testMode.exit());
     
     it('enques a job validating correct data', () => {
-        const createPushNotificationsJobs = require('./8-job');
         const list = [
             {
                 phoneNumber: '4153518780',
